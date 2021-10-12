6e57765f7edad10785602ce4222f03e0535c10a4
kent831217 <kent831217@gmail.com>
2021-10-12 14:51:42 +0800

    1012

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def callback():
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
     message = ImageSendMessage(
-        original_content_url='"https://example.com/original.jpg",',
+        original_content_url="https://example.com/original.jpg",
         preview_image_url='https://example.com/preview.jpg'
     )
     line_bot_api.reply_message(event.reply_token, message)
